4c3f8ee4930e4455797a37b7c5a80b50606e2bbb
MichalSmyk <michal.smyk39@gmail.com>
2022-12-08 11:18:52 +0000

    debugged

--- ex_2.rb
+++ ex_2.rb
@@ -1,5 +1,5 @@
 def encode(plaintext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
     ciphertext_chars = plaintext.chars.map do |char|
       (65 + cipher.find_index(char)).chr
     end
@@ -7,9 +7,9 @@ def encode(plaintext, key)
   end
   
   def decode(ciphertext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
     plaintext_chars = ciphertext.chars.map do |char|
-      cipher[65 - char.ord]
+        cipher[char.ord - 65]
     end
     return plaintext_chars.join
   end
@@ -22,3 +22,6 @@ def encode(plaintext, key)
 #
 # > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # => "theswiftfoxjumpedoverthelazydog"
+
+
+puts decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
